fb925804fa56990861c13d038df65ed9d01a065d
Hubert Kasinski <hubertkasinski17@gmail.com>
2021-06-18 16:34:59 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,25 +1,29 @@
-import pandas as pd 
-import numpy as np
-import matplotlib
-import cryptocompare as cp
-import datetime 
+#!/usr/bin/python
+
+import pandas as pd
 import requests
-import json
 from datetime import datetime
 
-trades= pd.read_csv('C:\\Users\\kille\\Documents\\Python project\\Portoflio screener\Trade_history.csv',header=0, index_col=0, parse_dates=False, na_values=-99.99)
-trades #Reading in your trade history
+trades = pd.read_csv('trade-history.csv', header=0, index_col=0, parse_dates=False, na_values=-99.99)
+#Reading in your trade history and changing dates to YYYY-MM-DD
 
-def crypto_amount(from_sym='',account=''):
-    deps = trades.loc[trades['To']==from_sym] #Separating buys from sells
-    wdrwls = trades.loc[trades['From']==from_sym]
+
+# Calculates how much of each asset we have left after all the trades.
+def crypto_amount(from_sym='', end_date='', start_date=trades.index.min(), account=''):
+    df = trades
+    if end_date:
+        df = trades.loc[start_date:end_date]
+    deps = df.loc[df['To'] == from_sym]
+    wdrwls = df.loc[df['From'] == from_sym]
     if account:
-        deps = deps.loc[deps['Account']==account]
-        wdrwls = wdrwls.loc[wdrwls['Account']==account]
-        
-    amount= (deps['Amount'].sum())-(wdrwls['Paid'].sum())
+        deps = deps.loc[deps['Account'] == account]
+        wdrwls = wdrwls.loc[wdrwls['Account'] == account]
+
+    amount = (deps['Amount'].sum()) - (wdrwls['Paid'].sum())
     return amount
 
+# Pulls the current price as a float in desired currency, using the cryptocompare api.
+# https://tcoil.info/how-to-get-price-data-for-bitcoin-and-cryptocurrencies-with-python-json-restful-api/
 def get_current_price(from_sym='BTC', to_sym='USD', exchange=''):
     url = 'https://min-api.cryptocompare.com/data/price'    
     
@@ -35,7 +39,8 @@ def get_current_price(from_sym='BTC', to_sym='USD', exchange=''):
     data = response.json()[to_sym]
     
     return data
-    
+
+#Using the crypto_amount and get_current price functions, it calculates the value of the crypto asset in desired currency
 def crypto_value(from_sym='BTC', to_sym='USD', exchange='', account=''):
     url = 'https://min-api.cryptocompare.com/data/price'    
     
@@ -56,13 +61,17 @@ def crypto_value(from_sym='BTC', to_sym='USD', exchange='', account=''):
     data = response.json()[to_sym]
     amount= (deps['Amount'].sum())-(wdrwls['Paid'].sum())
     return data*amount  
-     
+
+#Builds a dataframe of all the assets in the portfolio, with the amount, market price and value of each.
+# Value and market price are in desired currency, in this example I have used GBP.
+# Note: Defining the exchange of where each asset is held is not necessary, as the default exchange provides
+# a better current price inline with the exchange online, than when defining the exchange.
 s=sorted(set(trades['To'].to_list()))
 df=pd.DataFrame(list(s))
 del df[0]
 df['Asset']=pd.DataFrame(list(s))
-df['Amount'] = list(map(lambda x: crypto_amount(x),df['Asset']))
-df['Market Price']=list(map(lambda x: get_current_price(x,'GBP'),df['Asset'])) #I use GBP as it is my home currency however this works with USD and other currencies too!
-df['GBP Value'] =list(map(lambda x: crypto_value(x,'GBP'),df['Asset']))
+df['Amount'] = list(map(lambda x: crypto_amount(x), df['Asset']))
+df['Market Price']=list(map(lambda x: get_current_price(x, 'GBP'), df['Asset']))
+df['GBP Value'] =list(map(lambda x: crypto_value(x, 'GBP'), df['Asset']))
 print("Profit/Loss is £", df['GBP Value'].sum(), "as of", datetime.now())
 print(df)

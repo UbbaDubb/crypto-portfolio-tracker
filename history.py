adff7ed75f7a83a700aaf787ff5f994327a6f5a8
Hubert Kasinski <hubertkasinski17@gmail.com>
2021-06-24 13:34:46 +0100

    Create history.py

---
+++ history.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import crypto_portfolio_kit as ck
+import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+trades = pd.read_csv('trade-history.csv', header=0, index_col=0, na_values=-99.99)
+
+# %%time
+accounts = sorted(set(trades['Account'].to_list()))
+d = {}
+for name in accounts:
+    try:
+        d[name] = ck.portfolio(trades, account=name, exchange=name)
+    except:
+        d[name] = ck.portfolio(trades, account=name)
+
+
+for name in accounts:
+    d[name] = d[name].fillna(0)
+
+fp = sum(d.values())
+#Converts the portfolio history to desired currency, however it can be left in USDT if desired.
+fp = ck.convert(fp, 'USDT', 'GBP')
+
+print(fp)
+fp.to_csv("portfolio-history.csv")
+
+

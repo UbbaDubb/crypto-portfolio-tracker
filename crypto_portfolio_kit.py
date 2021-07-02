a8be51303c52f16a9c25981c0933169017aea973
Hubert Kasinski <hubertkasinski17@gmail.com>
2021-07-02 09:46:09 +0100

    Update crypto_portfolio_kit.py

--- crypto_portfolio_kit.py
+++ crypto_portfolio_kit.py
@@ -6,9 +6,9 @@ from datetime import datetime
 
 # Calculates how much of each asset we have left after all the trades.
 def crypto_amount(df, from_sym='', end_date='', start_date='', account=''):
-    start_date=df.index.min()
+    start_date = df.index.min()
     if end_date:
-        df=df.loc[start_date:end_date]
+        df = df.loc[start_date:end_date]
     deps = df.loc[df['To'] == from_sym]
     wdrwls = df.loc[df['From'] == from_sym]
     if account:
@@ -39,7 +39,7 @@ def get_current_price(from_sym='BTC', to_sym='USD', exchange=''):
 
 
 # Using the crypto_amount and get_current price functions, it calculates the value of the crypto asset in desired currency
-def crypto_value(df,from_sym='BTC', to_sym='USD', exchange='', account=''):
+def crypto_value(df, from_sym='BTC', to_sym='USD', exchange='', account=''):
     url = 'https://min-api.cryptocompare.com/data/price'
 
     deps = df.loc[df['To'] == from_sym]
@@ -96,24 +96,22 @@ def get_hist_close(from_sym='BTC', to_sym='USDT', timeframe='day', limit=2000, a
 
 # Using most of the previous functions, it calculates a historical porotfolio for a desired account and exchange in USDT.
 def portfolio(th, account='', exchange='', timeframe='day'):
-    global USDT
+    #global USDT
     assets = sorted(set(th['To'].to_list()))
     start_dt = th.index.min()
     end_dt = datetime.today().strftime('%Y-%m-%d')
 
     if account:
         th = th.loc[th['Account'] == account]
-    tl = pd.date_range(start_dt, end_dt).strftime("%Y-%m-%d") #Defines the date range
+    tl = pd.date_range(start_dt, end_dt).strftime("%Y-%m-%d")  # Defines the date range
 
-    #Creates a dataframe of the different amounts held over the date range
+    # Creates a dataframe of the different amounts held over the date range
     holdings = pd.DataFrame(np.zeros((len(tl), len(assets))))
     holdings = holdings.set_index(tl)
     holdings.columns = assets
-    for asset in assets:
-        #holdings[asset] = list(map(lambda x: crypto_amount(th, asset, x), holdings.index.strftime("%Y-%m-%d")))
 
-        for date in holdings.index:
-            holdings[asset][date] =crypto_amount(df=th, from_sym=asset, end_date=date, start_date=start_dt)
+    for asset in assets:
+        holdings[asset] = list(map(lambda x: crypto_amount(th, asset, x), holdings.index))
     if 'USDT' in holdings.columns:
         USDT = holdings['USDT']
         del holdings['USDT']
@@ -122,15 +120,13 @@ def portfolio(th, account='', exchange='', timeframe='day'):
         assets.remove('USDT')
     holdings.fillna(0)
 
-    #Creates a dataframe for the historical prices of the assets
+    # Creates a dataframe for the historical prices of the assets
     prices_hist = pd.DataFrame(np.zeros((len(tl), len(assets))))
     prices_hist = prices_hist.set_index(tl)
     prices_hist.columns = assets
-    if exchange:
-        for asset in assets:
-            prices_hist[asset] = (get_hist_close(asset, limit=(len(tl) - 1), exchange=exchange, timeframe=timeframe)['close']).to_numpy()
-
-    #Creates a final dataframe, which is the product of the two previous ones.
+    for asset in assets:
+        prices_hist[asset] = list((get_hist_close(asset, limit=(len(tl) - 1), exchange=exchange, timeframe=timeframe)['close']))
+    # Creates a final dataframe, which is the product of the two previous ones.
     portfolio_hist = prices_hist * holdings
     if 'USDT' in assets:
         portfolio_hist['USDT'] = USDT
@@ -142,7 +138,8 @@ def portfolio(th, account='', exchange='', timeframe='day'):
     portfolio_hist.fillna(0)
     return portfolio_hist
 
+
 def convert(pt, from_sym='USDT', to_sym='', exchange=''):
-    z=get_hist_close(from_sym, to_sym , limit=len(pt)-1).to_numpy()
-    df=pt.multiply(z, axis='rows')
+    z = get_hist_close(from_sym, to_sym, limit=len(pt) - 1).to_numpy()
+    df = pt.multiply(z, axis='rows')
     return df
